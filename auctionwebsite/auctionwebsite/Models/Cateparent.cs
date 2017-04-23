e4b466d1ccf0fcf395a6e7889c98375746dcee20
phucduye53 <phucduye53@gmail.com>
2017-04-23 21:09:18 +0700

    Add Cateparent Entity

---
+++ auctionwebsite/auctionwebsite/Models/Cateparent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace auctionwebsite.Models
+{
+    public class Cateparent
+    {
+        public int CateparentID { get; set; }
+        public string CateparentName { get; set; }
+        public virtual ICollection<Cate> cates { get; set; }
+    }
+}
