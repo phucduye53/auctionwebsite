c8dbc05f2b1f54f88ec9a0ebc975c2bc7d60f7fa
phucduye53 <phucduye53@gmail.com>
2017-04-23 21:09:10 +0700

    Add Category Entity

---
+++ auctionwebsite/auctionwebsite/Models/Cate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace auctionwebsite.Models
+{
+    public class Cate
+    {
+        public int CateID { get; set; }
+        public string CateName { get; set; }
+        public int CateparentID { get; set; }
+        public virtual Cateparent Cateparent { get; set; }
+    }
+}
