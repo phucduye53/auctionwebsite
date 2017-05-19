92ace8f8b5759a4e5d7f85deb6a7aed47137cfd3
phucduye53 <phucduye53@gmail.com>
2017-05-19 20:05:25 +0700

    check login

---
+++ auctionwebsite/auctionwebsite/Helpers/CheckLoginAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace auctionwebsite.Helpers
+{
+    public class CheckLoginAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (CurrentContext.IsLogged() == false)
+            {
+
+
+                filterContext.Result =
+                    new RedirectResult("~/User/Login");
+                return;
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
