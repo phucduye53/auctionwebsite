2be4a5fe9418abc40b161cc4ca6bc718d34d135f
phucduye53 <phucduye53@gmail.com>
2017-06-02 04:12:19 +0700

    add some element

--- auctionwebsite/auctionwebsite/Models/Product.cs
+++ auctionwebsite/auctionwebsite/Models/Product.cs
@@ -13,16 +13,24 @@ namespace auctionwebsite.Models
         [StringLength(50, ErrorMessage = "Tên sản phẩm chỉ được ít hơn 50 ký tự.")]
         public string ProductName { get; set; }
         [Required(ErrorMessage = "Chưa nhập giá sản phẩm")]
-        public string ProductPrice { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Giá sản phẩm phải là số")]
+        public int ProductPrice { get; set; }
+        [Required(ErrorMessage = "Chưa nhập bước giá sản phẩm")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Bước giá sản phẩm phải là số")]
+        public int ProductTickSize { get; set; }
         public string ProductSoldPrice { get; set; }
         public string ProductDes{ get; set; }
         public DateTime ProductDateSold { get; set; }
 
         [Required(ErrorMessage = "Chưa chọn danh mục .")]
         public int CateID { get; set; }
+        [Required(ErrorMessage = "Chưa chọn danh mục cha.")]
+        public int CateparentID { get; set; }
         public int UserUploadID { get; set; }
         public int UserBuyID { get; set; }
+        public int ProductPointRequired { get; set; }
         public virtual Cate Cate { get; set; }
+        public virtual Cate CateParent { get; set; }
         public virtual User User { get; set; }
         public virtual ICollection<FileDetail> FileDetails { get; set; }
     }
