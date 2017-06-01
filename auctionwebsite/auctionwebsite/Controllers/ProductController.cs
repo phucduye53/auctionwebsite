8d1851fbc6b40571386a23f875869a92f860f925
phucduye53 <phucduye53@gmail.com>
2017-06-02 04:13:15 +0700

    product controller

---
+++ auctionwebsite/auctionwebsite/Controllers/ProductController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using auctionwebsite.Models;
+using auctionwebsite.DAL;
+
+namespace auctionwebsite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private AuctionContext db = new AuctionContext();
+
+        // GET: /Product/
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Cate);
+            return View(products.ToList());
+        }
+
+        // GET: /Product/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: /Product/Create
+        public ActionResult Create()
+        {
+            ViewBag.CateParentID = new SelectList(db.Cateparents, "CateparentID", "CateparentName");
+            return View();
+        }
+
+        // POST: /Product/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ProductID,ProductName,ProductPrice,ProductSoldPrice,ProductDes,ProductDateSold,CateID,UserUploadID,UserBuyID,ProductPointRequired")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CateID = new SelectList(db.Cates, "CateID", "CateName", product.CateID);
+            return View(product);
+        }
+
+        // GET: /Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CateID = new SelectList(db.Cates, "CateID", "CateName", product.CateID);
+            return View(product);
+        }
+
+        // POST: /Product/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ProductID,ProductName,ProductPrice,ProductSoldPrice,ProductDes,ProductDateSold,CateID,UserUploadID,UserBuyID,ProductPointRequired")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CateID = new SelectList(db.Cates, "CateID", "CateName", product.CateID);
+            return View(product);
+        }
+
+        // GET: /Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: /Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public JsonResult GetCates(int id) 
+        {
+            List<Cate> Cates = new List<Cate>();
+            Cates = (from s in db.Cates
+                         where s.CateparentID==id
+                         select s).ToList();
+            return Json(new SelectList(Cates, "CateID", "CateName"));
+        }
+    }
+}
