4e2385d257a88366f4e68c2754270a1b760a1a8f
mypower119 <doit.programer@gmail.com>
2017-06-13 16:56:40 +0700

    add models detail, favorite, login

---
+++ auctionwebsite/Models/FileDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace auctionwebsite.Models
+{
+    public class FileDetail
+    {
+        public int FileDetailID { get; set; }
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+        public int ProductID { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
