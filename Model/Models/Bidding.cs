1f18b2c25d6ee3374552804a3a8716da1347d67a
ThaiNguyen2709 <nguyenthai27091996@gmail.com>
2017-06-18 21:56:23 +0700

    Model

---
+++ Model/Models/Bidding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace auctionwebsite.Models
+{
+    public class Bidding
+    {
+        public int BiddingID { get; set; }
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public string DateBid { get; set; }
+        public int PriceBid { get; set; }
+        public int ProductBid { get; set; }
+        public int BidStatus { get; set; }
+        public virtual User User { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ Model/Models/Favorite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace auctionwebsite.Models
+{
+    public class Favorite
+    {
+        public int FavoriteID { get; set; }
+        public int UserID { get; set; }
+        public int ProductID { get; set; }
+        public virtual User User { get; set; }
+        public virtual Product Product { get; set; }
+
+    }
+}
