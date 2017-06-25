2543786dc903bf75e019033da30ae8858954be18
phucduye53 <phucduye53@gmail.com>
2017-06-25 11:10:07 +0700

    auto send mail

--- auctionwebsite/auctionwebsite/Controllers/ProductController.cs
+++ auctionwebsite/auctionwebsite/Controllers/ProductController.cs
@@ -137,45 +137,47 @@ namespace auctionwebsite.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [CheckLogin(Permission = 0)]
-                public ActionResult Edit([Bind(Include = "ProductID,ProductSoldInstantPrice,ProductName,ProductPrice,ProductSoldPrice,ProductDateSold,ProductTickSize,ProductDes,CateID,CateparentID,UserID,ProductPointRequired")] Product product, List<HttpPostedFileBase> fileSubInput)
+        public ActionResult Edit([Bind(Include = "ProductID,ProductSoldInstantPrice,ProductName,ProductPrice,ProductSoldPrice,ProductDateSold,ProductTickSize,ProductDes,CateID,CateparentID,UserID,ProductPointRequired")] Product product, List<HttpPostedFileBase> fileSubInput)
+        {
+            if (ModelState.IsValid)
                 {
-                    if (ModelState.IsValid)
+                    db.Entry(product).State = EntityState.Modified;
+                    db.SaveChanges();
+                    Product temp = db.Products.Include(s => s.FileDetails).SingleOrDefault(x => x.ProductID == product.ProductID);
+                    var count =  temp.FileDetails.Count();
+                    var countSub = 0;
+                    foreach (HttpPostedFileBase file in fileSubInput)
                     {
-                        db.Entry(product).State = EntityState.Modified;
-                        db.SaveChanges();
-                        Product temp = db.Products.Include(s => s.FileDetails).SingleOrDefault(x => x.ProductID == product.ProductID);
-                        var count =  temp.FileDetails.Count();
-                        var countSub = 0;
-                        foreach (HttpPostedFileBase file in fileSubInput)
+                        countSub++;
+                    }
+                    if (count+countSub>=4)
+                    {
+                        ViewBag.Message = "Ảnh phụ không được quá 3 tấm";
+                        return View(product);
+
+                    }
+                    foreach (HttpPostedFileBase file in fileSubInput)
+                    {
+                        if (file != null)
                         {
-                            countSub++;
-                        }
-                        if (count+countSub>=4)
-                        {
-                            ViewBag.Message = "Ảnh phụ không được quá 3 tấm";
-                        }
-                        foreach (HttpPostedFileBase file in fileSubInput)
-                        {
-                            if (file != null)
+                            FileDetail fileDetail = new FileDetail()
                             {
-                                FileDetail fileDetail = new FileDetail()
-                                {
-                                    FileName = Path.GetFileName(file.FileName),
-                                    Extension = Path.GetExtension(Path.GetFileName(file.FileName)),
-                                    ProductID = product.ProductID
-                                };
-                                db.Entry(fileDetail).State = EntityState.Added;
-                                db.SaveChanges();
-                                var pathSub = Path.Combine(Server.MapPath("~/Img/"), fileDetail.ProductID + "_" + fileDetail.FileDetailID + "_" + fileDetail.FileName);
-                                file.SaveAs(pathSub);
-                            }
-                        }
+                                FileName = Path.GetFileName(file.FileName),
+                                Extension = Path.GetExtension(Path.GetFileName(file.FileName)),
+                                ProductID = product.ProductID
+                            };
+                            db.Entry(fileDetail).State = EntityState.Added;
+                            db.SaveChanges();
+                            var pathSub = Path.Combine(Server.MapPath("~/Img/"), fileDetail.ProductID + "_" + fileDetail.FileDetailID + "_" + fileDetail.FileName);
+                            file.SaveAs(pathSub);
+                         }
+                     }
                         return RedirectToAction("Index");
                     }
 
-                    ViewBag.CateParentID = new SelectList(db.Cateparents, "CateparentID", "CateparentName");
-                    return View(product);
-                }
+                 ViewBag.CateParentID = new SelectList(db.Cateparents, "CateparentID", "CateparentName");
+                 return View(product);
+         }
 
         // GET: /Product/Delete/5
         [CheckLogin(Permission = 0)]
@@ -235,15 +237,15 @@ namespace auctionwebsite.Controllers
                     return Json(new { Result = "Error" });
                 }
                 //Remove from database
-                db.FileDetails.Remove(fileDetail);
-                db.SaveChanges();
-
-                //Delete file from the file system
                 var pathSub = Path.Combine(Server.MapPath("~/Img/"), fileDetail.ProductID + "_" + fileDetail.FileDetailID + "_" + fileDetail.FileName);
                 if (System.IO.File.Exists(pathSub))
                 {
                     System.IO.File.Delete(pathSub);
                 }
+                db.FileDetails.Remove(fileDetail);
+                db.SaveChanges();
+
+                //Delete file from the file system
                 return Json(new { Result = "OK" });
             }
             catch (Exception ex)
@@ -280,22 +282,16 @@ namespace auctionwebsite.Controllers
         [CheckLogin(Permission = 0)]
         public ActionResult Bidding(int userid, int proid, int price)
         {
-            //try
-            //{
             Product product = db.Products.Include(s => s.Biddings).Include(s => s.FileDetails).Include(s => s.User).Include(p => p.Favorites).Include(p => p.Biddings).SingleOrDefault(x => x.ProductID == proid);
-            //if (product.ProductPrice > price)
-            //{
-            //        return Json(new { Result = "Lower" });
-            //}
-            Bidding temp = db.Biddings.Where(p => p.ProductID == proid && p.ProductBid == product.ProductPrice).FirstOrDefault();
-
+            Bidding tempbid = db.Biddings.Where(p => p.PriceBid == product.ProductPrice && p.ProductID == proid).FirstOrDefault();
+            var tempprice = product.ProductPrice;
             product.ProductPrice = product.ProductPrice + product.ProductTickSize;
             Bidding bid = new Bidding()
             {
                 UserID = userid,
                 ProductID = proid,
                 PriceBid = price,
-                ProductBid = product.ProductPrice + product.ProductTickSize,
+                ProductBid = product.ProductPrice,
                 DateBid = DateTime.Now.ToString()
             };
             db.Entry(product).State = EntityState.Modified;
@@ -303,43 +299,61 @@ namespace auctionwebsite.Controllers
 
             db.SaveChanges();
             //Email for Buyer
-            //try
-            //{
-            //    string content = System.IO.File.ReadAllText(Server.MapPath("~/MailTemplate/Bidding/Buyer.html"));
-                
+            try
+            {
+                string content = System.IO.File.ReadAllText(Server.MapPath("~/MailTemplate/Bidding/Buyer.html"));
+                User tempuser = db.Users.Where(s => s.UserID == userid).FirstOrDefault();
+                string toBuyerEmail = tempuser.UserEmail;
 
-            //    content = content.Replace("{{ProductName}}", product.ProductName);
-            //    content = content.Replace("{{ProductID}}", product.ProductID.ToString());
-            //    content = content.Replace("{{UserName}}", product.User.UserName);
-            //    content = content.Replace("{{ProductPrice}}", bid.PriceBid.ToString());
-            //    content = content.Replace("{{ProductRealPrice}}", product.ProductPrice.ToString());
-            //    if (bid.User == null)
-            //    {
-             
-            //    }
-            //    else
-            //    {
-            //        MailHelper.SendMail(bid.User.UserEmail.ToString(), "Đấu giá sản phẩm - Người mua", content);
-            //    }
+                content = content.Replace("{{ProductName}}", product.ProductName);
+                content = content.Replace("{{ProductID}}", product.ProductID.ToString());
+                content = content.Replace("{{UserName}}", product.User.UserName);
+                content = content.Replace("{{ProductPrice}}", bid.PriceBid.ToString());
+                content = content.Replace("{{ProductRealPrice}}", product.ProductPrice.ToString());
+                if (tempuser == null)
+                {
 
-                //MailHelper.SendMail(toEmail, "Đấu giá sản phẩm - Người mua", content);
+                }
+                else
+                {
+                    MailHelper.SendMail(toBuyerEmail, "Đấu giá sản phẩm - Người mua", content);
+                }
 
-                ////Email for Seller
-                //string content2 = System.IO.File.ReadAllText(Server.MapPath("~/MailTemplate/Bidding/Seller.html"));
 
-                //content2 = content2.Replace("{{ProductName}}", product.ProductName);
-                //content2 = content2.Replace("{{ProductID}}", product.ProductID.ToString());
-                //content2 = content2.Replace("{{UserName}}", product.User.UserName);
-                //content2 = content2.Replace("{{ProductPrice}}", bid.PriceBid.ToString());
-                //content2 = content2.Replace("{{UserBuyName}}", bid.User.UserName.ToString());
+                //Email for Seller
+                string content2 = System.IO.File.ReadAllText(Server.MapPath("~/MailTemplate/Bidding/Seller.html"));
+                User tempuser2 = db.Users.Where(s => s.UserID == product.User.UserID).FirstOrDefault();
+                string toSellerEmail = tempuser2.UserEmail;
 
-                //MailHelper.SendMail(product.User.UserEmail.ToString(), "Đấu giá sản phẩm - Người bán", content2);
-            //}
-            //catch(Exception ex)
-            //{
-            //    TempData["message"] = ex;
-            //    return RedirectToAction("Index", "Home");
-            //}
+                content2 = content2.Replace("{{ProductName}}", product.ProductName);
+                content2 = content2.Replace("{{ProductID}}", product.ProductID.ToString());
+                content2 = content2.Replace("{{UserName}}", product.User.UserName);
+                content2 = content2.Replace("{{ProductPrice}}", bid.PriceBid.ToString());
+                content2 = content2.Replace("{{UserBuyName}}", bid.User.UserName.ToString());
+
+                MailHelper.SendMail(toSellerEmail, "Đấu giá sản phẩm - Người bán", content2);
+
+
+                //Email for Buyer - Highest Price
+                if (tempbid != null && tempbid.UserID != tempuser.UserID )
+                {
+                    string content3 = System.IO.File.ReadAllText(Server.MapPath("~/MailTemplate/Bidding/LastHigestBuyer.html"));
+                    User tempuser3 = db.Users.Where(s => s.UserID == tempbid.UserID).FirstOrDefault();
+                    string toBuyerHighestEmail = tempuser3.UserEmail;
+                    content2 = content2.Replace("{{ProductName}}", product.ProductName);
+                    content2 = content2.Replace("{{ProductID}}", product.ProductID.ToString());
+                    content2 = content2.Replace("{{UserName}}", product.User.UserName);
+                    content2 = content2.Replace("{{ProductPrice}}", bid.PriceBid.ToString());
+                    content2 = content2.Replace("{{ProductLastPrice}}", tempbid.PriceBid.ToString());
+                    MailHelper.SendMail(toBuyerHighestEmail, "Đấu giá sản phẩm - Người giữ giá cao ", content3);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                TempData["message"] = ex;
+                return RedirectToAction("Index", "Home");
+            }
 
 
             return PartialView("DetailPartial", product);
@@ -367,24 +381,91 @@ namespace auctionwebsite.Controllers
                 db.Entry(item).State = EntityState.Modified;
                 db.SaveChanges();
             }
-            int max = db.Biddings.Where(p=>p.BidStatus==0).Max(x=>x.PriceBid);
+            int max = db.Biddings.Where(p=>p.BidStatus==0 && p.ProductID==proid).Max(x=>x.ProductBid);
             if(max < product.ProductPrice)
             {
                 product.ProductPrice = max;
                 db.Entry(product).State = EntityState.Modified;
                 db.SaveChanges();
             }
-
+            string content = System.IO.File.ReadAllText(Server.MapPath("~/MailTemplate/Kick/KickUser.html"));
+            User tempuser = db.Users.Where(s => s.UserID == userid).FirstOrDefault();
+            string toSeller = tempuser.UserEmail;
+            content = content.Replace("{{ProductName}}", product.ProductName);
+            content = content.Replace("{{ProductID}}", product.ProductID.ToString());
+            content = content.Replace("{{UserName}}", product.User.UserName);
+            MailHelper.SendMail(toSeller, "Đấu giá sản phẩm - Người mua", content);
             return PartialView("DetailPartial", product);
         }
         [HttpPost]
-        public PartialViewResult BidOver(int proid)
+        public JsonResult BidOver(int proid)
         {
             Product product = db.Products.Include(s => s.Biddings).Include(s => s.FileDetails).Include(s => s.User).Include(p => p.Favorites).Include(p => p.Biddings).SingleOrDefault(x => x.ProductID == proid);
+            if (product.ProductStatus == 3)
+            {
+                return Json(new { Result = "OK" });
+            }
             product.ProductStatus = 3;
             db.Entry(product).State = EntityState.Modified;
             db.SaveChanges();
-            return PartialView("DetailPartial", product);
+            // Handle
+            var bidtemp = db.Biddings.Where(p => p.ProductID == proid).ToList();
+            if (bidtemp.Count() == 0)
+            {
+                string content = System.IO.File.ReadAllText(Server.MapPath("~/MailTemplate/BidOver/Seller.html"));
+                User tempuser = db.Users.Where(s => s.UserID == product.UserID).FirstOrDefault();
+                string toSellerEmail = tempuser.UserEmail;
+                content = content.Replace("{{ProductName}}", product.ProductName);
+                content = content.Replace("{{ProductID}}", product.ProductID.ToString());
+                content = content.Replace("{{UserName}}", product.User.UserName);
+                if (tempuser == null)
+                {
+
+                }
+                else
+                {
+                    MailHelper.SendMail(toSellerEmail, "Đấu giá sản phẩm - Người mua", content);
+                }
+            }
+            else
+            {
+                Bidding highest = db.Biddings.Where(s => s.ProductID == proid && product.ProductPrice == s.ProductBid).OrderByDescending(s => s.ProductBid).FirstOrDefault();
+                User tempBuyer = db.Users.Where(s => s.UserID == product.UserID).FirstOrDefault();
+                User tempSeller = db.Users.Where(s => s.UserID == highest.UserID).FirstOrDefault();
+                // Seller
+                string content = System.IO.File.ReadAllText(Server.MapPath("~/MailTemplate/BidOver2/Seller.html"));
+                string toSellerEmail = tempBuyer.UserEmail;
+                content = content.Replace("{{ProductName}}", product.ProductName);
+                content = content.Replace("{{ProductID}}", product.ProductID.ToString());
+                content = content.Replace("{{UserName}}", product.User.UserName);
+                content = content.Replace("{{UserBuyName}}", tempSeller.UserName);
+                content = content.Replace("{{ProductPrice}}", product.ProductPrice.ToString());
+                content = content.Replace("{{UserSellerEmail}}", product.User.UserEmail);
+
+                //buyer
+                string content2 = System.IO.File.ReadAllText(Server.MapPath("~/MailTemplate/BidOver2/Buyer.html"));
+
+                string toBuyerEmail = tempSeller.UserEmail;
+                content = content.Replace("{{ProductName}}", product.ProductName);
+                content = content.Replace("{{ProductID}}", product.ProductID.ToString());
+                content = content.Replace("{{UserName}}", product.User.UserName);
+                content = content.Replace("{{UserBuyName}}", tempSeller.UserName);
+                content = content.Replace("{{ProductPrice}}", product.ProductPrice.ToString());
+                content = content.Replace("{{UserBuyerEmail}}", tempBuyer.UserEmail);
+                if (tempSeller == null || tempSeller == null)
+                {
+
+                }
+                else
+                {
+                    MailHelper.SendMail(toSellerEmail, "Đấu giá sản phẩm - Người bán", content);
+                    MailHelper.SendMail(toBuyerEmail, "Đấu giá sản phẩm - Người mua", content2);
+
+                }
+            }
+
+
+            return Json(new { Result = "OK" });
         }
     }
 }
