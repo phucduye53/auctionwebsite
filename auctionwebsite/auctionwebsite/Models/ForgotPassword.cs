ab2da5da68e82f6ae59391d079ccb13383a0d1d1
phucduye53 <phucduye53@gmail.com>
2017-06-29 10:24:39 +0700

    model forgot password

---
+++ auctionwebsite/auctionwebsite/Models/ForgotPassword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace auctionwebsite.Models
+{
+    public class ForgotPassword
+    {
+        [Required(ErrorMessage = "Chưa nhập Email")]
+        [EmailAddress(ErrorMessage = "Nhập không đúng địa chỉ Email")]
+        [System.Web.Mvc.Remote("IsEmailNotExits", "User", ErrorMessage = "Email không tồn tại.")]
+        public string UserEmail { get; set; }
+    }
+}
